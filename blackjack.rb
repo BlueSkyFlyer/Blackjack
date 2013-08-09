60e6ef060f205519acc2f46fc8f0a5ccc95d7fc1
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-08-08 19:56:31 -0500

    Procedural blackjack

--- blackjack.rb
+++ blackjack.rb
@@ -25,23 +25,7 @@ def calculate_total(cards)
   total
 end
 
-def game hit_or_stay
-
-	if hit_or_stay == 1
-		mycards << deck.pop
-		calculate_total(mycards)
-		puts "You now have #{mytotal}"
-	elsif hit_or_stay == 2
-    dealercards << deck.pop
-    calculate_total(dealercards)
-    puts "The dealer has #{dealertotal}"
-  end
-end
-
-  	
-			
-
-
+# Start Game
 
 puts "Let's play Blackjack! What's your name?"
 
@@ -73,8 +57,92 @@ mytotal = calculate_total(mycards)
 puts "Dealer has: #{dealercards[0]} and #{dealercards[1]}, for a total of #{dealertotal}"
 puts "You have: #{mycards[0]} and #{mycards[1]} for a total of : #{mytotal}"
 puts " "
-puts "What would you like to do? 1) Hit 2) Stay"
-hit_or_stay = gets.chomp
 
-hit_or_stay.game
+
+# Player turn
+
+if mytotal == 21
+  puts "Congratulations, you hit blackjack! You win!"
+  exit
+end
+
+while mytotal < 21
+	puts "What would you like to do? 1) Hit 2) Stay"
+  hit_or_stay = gets.chomp
+
+  if !['1', '2'].include?(hit_or_stay)
+    puts "Error: you must enter 1 or 2"
+    next
+  end
+
+  # Stay
+  
+  if hit_or_stay == "2"
+  	puts "You chose to stay."
+    break
+  end
+
+  # Hit
+  new_card = deck.pop
+  puts "Dealing card to player: #{new_card}"
+  mycards << new_card
+  mytotal = calculate_total(mycards)
+  puts "Your total is now: #{mytotal}"
+
+  if mytotal == 21
+  	puts "Congratulations, you hit blackjack! You win!"
+  	exit
+  elsif mytotal > 21
+  	puts "Sorry - it looks like you busted!" 
+  	exit
+  end
+end
+
+# Dealer turn
+
+if dealertotal == 21
+	puts "Sorry, dealer hit blackjack. You lose"
+	exit
+end
+
+while dealertotal < 17
+	# Hit
+	new_card = deck.pop
+	puts "Dealing new card for dealer: #{new_card}"
+	dealercards << new_card
+	dealertotal = calculate_total(dealercards)
+	puts "Dealer total is now: #{dealertotal}"
+
+  if dealertotal == 21
+  	puts "Sorry, dealer hit blackjack. You lose."
+  	exit
+  elsif dealertotal > 21
+  	puts "Congratulations, dealer busted! You win!"
+  	exit
+  end
+end
+
+# Compare hands
+
+puts "Dealer's cards: "
+dealercards.each do |card|
+	puts "=> #{card}"
+end
+
+puts "Your cards: "
+mycards.each do |card|
+	puts "=> #{card}"
+end
+puts
+
+if dealertotal > mytotal
+	puts "Sorry, dealer won."
+elsif dealertotal < mytotal
+	puts "Congratulations, you win!"
+else
+	puts "It's a tie!"
+end
+
+exit
+
 
